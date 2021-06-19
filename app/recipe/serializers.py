fd4e096a30d9ff263d33fc14b04bdcc6f5626ce0
José Vitor Santos Silva <jose.silva@dcomp.ufs.br>
2021-06-19 10:09:57 -0300

    Added create ingredient feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagsSerializer(serializers.ModelSerializer):
@@ -8,5 +8,14 @@ class TagsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredient objects"""
+
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
